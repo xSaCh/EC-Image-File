dddd517c502ba5bd7cfc3d0f8020db982157f85e
xSaCh <samarthchauhan7@gmail.com>
2022-03-19 12:40:00 +0530

    Compression simplify

--- src/compression/comp.cpp
+++ src/compression/comp.cpp
@@ -1,20 +1,18 @@
 #include <iostream>
 #include <fstream>
-#include <map>
+#include <unordered_map>
 #include <vector>
 
-#define MAX_SIZE 4096
+#define MAX_SIZE 2 << 15
+#define EOF_CODE 256
 
 using namespace std;
 
-vector<uint32_t> compress(const string input)
+vector<uint32_t> compress(const string fileName)
 {
     // LZW Dictionary
-    map<string, uint32_t> codeTable;
+    unordered_map<string, uint32_t> codeTable;
     int codeC = 0; // Code Count for LZW Dict
-
-    //Sample string
-    // string sm = "ABBABBBABBA";
 
     //output data
     vector<uint32_t> output;
@@ -24,6 +22,12 @@ vector<uint32_t> compress(const string input)
     // Encoded String + next char
     string strPCode = "";
 
+    // Input File Handler
+    ifstream inpFile(fileName, ios::binary);
+
+    char c;
+    size_t fileSize = 0;
+
     // Initizalize Dictionary
     for (int i = 0; i < 256; i++)
     {
@@ -32,9 +36,10 @@ vector<uint32_t> compress(const string input)
     codeC = 257; // 256 is for EOF marker
 
     //step 2
-    for (int i = 0; i < input.size(); i++)
+    while (inpFile.get(c))
     {
-        strPCode = encodeStr + input[i];
+        fileSize++;
+        strPCode = encodeStr + c;
 
         if (codeTable.count(strPCode) > 0)
             encodeStr = strPCode;
@@ -47,30 +52,53 @@ vector<uint32_t> compress(const string input)
                 codeTable[strPCode] = codeC++;
             }
 
-            encodeStr = input[i];
+            encodeStr = c;
         }
     }
 
+    inpFile.close();
     if (codeTable.count(encodeStr) > 0)
         output.push_back(codeTable[encodeStr]);
     else
         output.push_back(codeC++);
 
-    cout << "Original Size   : " << input.size() << "\n";
+    cout << "\nOriginal Size   : " << fileSize << "\n";
     cout << "Compressed Size : " << output.size() << "\n";
-    cout << "Percentage      : " << 1 - ((double)output.size() / (double)input.size()) << "%\n";
+    cout << "LAST: " << codeC << codeTable[encodeStr.substr(0, encodeStr.size() - 1)] << " " << encodeStr.substr(0, encodeStr.size() - 1) << '\n';
+    cout << "Percentage      : " << 1 - ((double)output.size() / (double)fileSize) << "%\n";
 
     return output;
 }
 
-string decompress(const vector<uint32_t> input)
+bool getCode(ifstream &input, uint32_t &code)
+{
+    char c;
+    if (!input.get(c))
+        return false;
+    code = c & 0xff;
+
+    if (!input.get(c))
+        return false;
+
+    code |= (c & 0xff) << 8;
+    return code != EOF_CODE;
+}
+
+string decompress(const string fileName)
 {
     // LZW Dictionary
-    map<uint32_t, string> codeTable;
+    unordered_map<uint32_t, string> codeTable;
     int codeC = 0; // Code Count for LZW Dict
 
     //Encoded Data
     // vector<uint32_t> input = {65, 66, 66, 257, 258, 260, 65};
+
+    // Input File Handler
+    ifstream inpFile(fileName, ios::binary);
+    ofstream outFile("F:\\aa.bin", ios::binary);
+
+    char c;
+    size_t fileSize = 0;
 
     //output data
     string output = "";
@@ -82,27 +110,38 @@ string decompress(const vector<uint32_t> input)
         codeTable[i] = string(1, i);
 
     codeC = 257; // 256 is for EOF marker
+    uint32_t code = 0;
 
-    for (int i = 0; i < input.size(); i++)
+    while (getCode(inpFile, code))
     {
-        output += codeTable[input[i]];
+        if (codeTable.count(code) <= 0)
+            codeTable[codeC] = prevStr + prevStr[0];
+        output += codeTable[code];
+        outFile << codeTable[code];
+        if ((prevStr.size() > 0) && codeC < MAX_SIZE)
+            codeTable[codeC++] = prevStr + codeTable[code][0];
 
-        if (prevStr.size() > 0)
-            codeTable[codeC++] = prevStr + codeTable[input[i]][0];
-
-        prevStr = codeTable[input[i]];
+        prevStr = codeTable[code];
     }
 
-    cout << "STR: " << output << '\n';
+    inpFile.close();
+    outFile.close();
+    cout << "STR: " << output.size() << '\n';
 
     return output;
 }
 int main()
 {
+    vector<uint32_t> a = compress("F:\\a.bin");
 
-    // ifstream file("F:\\a.txt", ios::binary);
-    cout << "Hello😍\n";
+    ofstream ofi("F:\\b.bin", ios::binary);
 
-    decompress(compress(a));
+    for (int i = 0; i < a.size(); i++)
+    {
+        ofi.put(a[i] & 0xff);
+        ofi.put((a[i] >> 8) & 0xff);
+    }
+    ofi.close();
+    // decompress("F:\\b.bin");
     return 0;
 }
