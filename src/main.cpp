57367b2523604e8dffe73c8f7d7917cd9ede0d3d
xSaCh <samarthchauhan7@gmail.com>
2022-03-27 12:41:46 +0530

    Add Resize Mode

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,14 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
 
 #include "stb_image.h"
 #include "stb_image_write.h"
+#include "stb_image_resize.h"
+
+//Width of Img after Resize
+#define RESIZE_WIDTH 256
 
 using namespace std;
 
@@ -16,6 +21,7 @@ ECIImg img[2];
 
 // EL el;
 ECI *WriteEl(int imgNum, const string imgsPath[], const char *outFile);
+uint8_t *ImgResizeData(const uint8_t *img_data, const uint32_t imgSize);
 
 int main(int argc, char *args[])
 {
@@ -40,6 +46,26 @@ int main(int argc, char *args[])
             f.write((char *)nn->data, head.sizes[pos]);
             f.close();
         }
+        else if (strcmp(args[1], "-rs") == 0)
+        {
+            char *inpFile = args[2];
+            int pos = atoi(args[3]);
+            char *outFile = args[4];
+
+            //Read  args[3]
+            ECIHeader head;
+            ECIImg *nn = ECI::ReadAt(inpFile, pos, &head);
+            if (nn == NULL)
+            {
+                printf("Input file path doesn't exists.\n");
+                return 1;
+            }
+
+            uint8_t *reData = ImgResizeData(nn->data, head.sizes[pos]);
+
+            float scale = (RESIZE_WIDTH / (float)nn->w);
+            stbi_write_jpg(((string) "resize_" + outFile).c_str(), nn->w * scale, nn->h * scale, nn->channels, reData, 50);
+        }
         else if (strcmp(args[1], "-w") == 0)
         {
             char *outFile = args[2];
@@ -56,6 +82,7 @@ int main(int argc, char *args[])
     {
         printf("Usage \n");
         printf("For Read Mode  '-r', fileName, ImgIndex, outFileName.\n");
+        printf("For Read with resize Mode  '-rs', fileName, ImgIndex, outFileName.\n");
         printf("For Write Mode '-w', outFileName, ImgIndex, imgFileNames. \n");
     }
 
@@ -88,4 +115,29 @@ ECI *WriteEl(int imgNum, const string imgsPath[], const char *outFile)
     }
     el->Write(outFile);
     return el;
+}
+
+uint8_t *ImgResizeData(const uint8_t *img_data, const uint32_t imgSize)
+{
+    int x, y, c;
+    uint8_t *imdDa = stbi_load_from_memory(img_data, imgSize, &x, &y, &c, 0);
+
+    float scale = RESIZE_WIDTH / (float)x;
+
+    uint8_t *data = (uint8_t *)malloc(sizeof(uint8_t) * x * y * c * scale);
+
+    int code = stbir_resize_uint8(imdDa, x, y, 0, data, x * scale, y * scale, 0, c);
+
+    if (code == 0)
+    {
+        cout << "RESIZE BULL\n";
+        free(data);
+        return NULL;
+    }
+
+    img->w = x * scale;
+    img->h = y * scale;
+    img->channels = c;
+
+    return data;
 }
